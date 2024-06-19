886161bf3c0f5203bf874993b25658696281ff93
KatyaBahai <katyxa-two@yandex.ru>
2024-06-19 19:10:27 +0300

    project structure change, attempt 4

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,4 +1,4 @@
-package src.main.java.ru.job4j.dreamjob.controller;
+package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
